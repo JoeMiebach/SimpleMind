d0c4d4a2e1d59fc33c5a7cac471a5d2d13fa38f8
Joe Miebach <joemiebach2@gmail.com>
2020-05-04 14:42:29 +0200

    Snapping

--- Webseite/script.js
+++ Webseite/script.js
@@ -14,19 +14,58 @@ let xOffsetOther, yOffsetOther;
 //Cords of the Outside Elem
 let xOutside, yOutside;
 
-function select(elem) {
-    console.log("grab");
-    elemSelected = elem;
-    xOffsetCircle = mouse.x + document.getElementById("workingArea").scrollLeft - elemSelected.getAttribute("cx");
-    yOffsetCircle = mouse.y + document.getElementById("workingArea").scrollTop - elemSelected.getAttribute("cy");
+let snappingDistance = 100;
 
-    xOffsetOther = mouse.x + document.getElementById("workingArea").scrollLeft - elemSelected.getAttribute("x");
-    yOffsetOther = mouse.y + document.getElementById("workingArea").scrollTop - elemSelected.getAttribute("y");
+function select(elem) {
+
+    if(elemSelected == null) {
+        console.log("grab");
+        elemSelected = elem;
+        xOffsetCircle = mouse.x + document.getElementById("workingArea").scrollLeft - elemSelected.getAttribute("cx");
+        yOffsetCircle = mouse.y + document.getElementById("workingArea").scrollTop - elemSelected.getAttribute("cy");
+        
+        xOffsetOther = mouse.x + document.getElementById("workingArea").scrollLeft - elemSelected.getAttribute("x");
+        yOffsetOther = mouse.y + document.getElementById("workingArea").scrollTop - elemSelected.getAttribute("y");
+        
+        
+        
+        const selectionVisualisation = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        
+        selectionVisualisation.setAttribute("x", elemSelected.getBoundingClientRect().x + document.getElementById("workingArea").scrollLeft);
+        selectionVisualisation.setAttribute("y", elemSelected.getBoundingClientRect().y + document.getElementById("workingArea").scrollTop);
+        
+        selectionVisualisation.setAttribute("width", elemSelected.getBoundingClientRect().width);
+        selectionVisualisation.setAttribute("height", elemSelected.getBoundingClientRect().height);
+        selectionVisualisation.setAttribute("stroke", "gray");
+        selectionVisualisation.setAttribute("stroke-width", "1");
+        selectionVisualisation.setAttribute("stroke-opacity", "0.5");
+        selectionVisualisation.setAttribute("stroke-linejoin", "round");
+        selectionVisualisation.setAttribute("fill", "none");
+        selectionVisualisation.setAttribute("id", "selectionVisualisation")
+        document.getElementById("ID_SVG").appendChild(selectionVisualisation);
+
+
+
+
+
+        const dragTopLeft = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+        dragTopLeft.setAttribute("cx", elemSelected.getBoundingClientRect().x);
+        dragTopLeft.setAttribute("cy", elemSelected.getBoundingClientRect().y);
+
+        dragTopLeft.setAttribute("r", 30);
+        dragTopLeft.setAttribute("fill", "blue");
+
+        console.log(dragTopLeft);
+        document.getElementById("ID_SVG").appendChild(dragTopLeft);
+
+    }
 }
 
 
 function deselect(elem) {
     elemSelected = null;
+    document.getElementById("selectionVisualisation").remove();
 }
 
 
@@ -46,12 +85,16 @@ function findScreenCoords(mouseEvent) {
     
 
     if (elemSelected != null) {
-        elemSelected.setAttribute('cx', mouse.x + document.getElementById("workingArea").scrollLeft - xOffsetCircle);
-        elemSelected.setAttribute('cy', mouse.y + document.getElementById("workingArea").scrollTop - yOffsetCircle);
+        elemSelected.setAttribute('cx', (Math.round((mouse.x + document.getElementById("workingArea").scrollLeft - xOffsetCircle) / snappingDistance)) * snappingDistance);
+        elemSelected.setAttribute('cy', (Math.round((mouse.y + document.getElementById("workingArea").scrollTop - yOffsetCircle) / snappingDistance)) * snappingDistance);
+        console.log(((Math.round((mouse.x + document.getElementById("workingArea").scrollLeft - xOffsetCircle) / snappingDistance)) * snappingDistance) + " " + (Math.round((mouse.y + document.getElementById("workingArea").scrollTop - yOffsetCircle) / snappingDistance)) * snappingDistance);
 
-        elemSelected.setAttribute('x', mouse.x + document.getElementById("workingArea").scrollLeft - xOffsetOther);
-        elemSelected.setAttribute('y', mouse.y + document.getElementById("workingArea").scrollTop - yOffsetOther);
+        elemSelected.setAttribute('x', (Math.round((mouse.x + document.getElementById("workingArea").scrollLeft - xOffsetOther) / snappingDistance)) * snappingDistance);
+        elemSelected.setAttribute('y', (Math.round((mouse.y + document.getElementById("workingArea").scrollTop - yOffsetOther) / snappingDistance)) * snappingDistance);
 
+
+        document.getElementById("selectionVisualisation").setAttribute("x", elemSelected.getBoundingClientRect().x + document.getElementById("workingArea").scrollLeft)
+        document.getElementById("selectionVisualisation").setAttribute("y", elemSelected.getBoundingClientRect().y + document.getElementById("workingArea").scrollTop);
 
         resizeSVG();
     }
