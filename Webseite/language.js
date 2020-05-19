685352367be5f28de5c2bf7432b01d812fffbe6d
Stheini <61031149+Stheini@users.noreply.github.com>
2020-05-19 20:46:32 +0200

    Color Picker Js

--- Webseite/language.js
+++ Webseite/language.js
@@ -1,11 +1,11 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        $.getJSON('http://ws.geonames.org/countryCode', {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-            type: 'JSON'
-        }, function(result) {
-            alert('Country: ' + result.countryName + '\n' + 'Code: ' + result.countryCode);
-        });
-    });
-}​
+//if (navigator.geolocation) {
+//    navigator.geolocation.getCurrentPosition(function(position) {
+//        $.getJSON('http://ws.geonames.org/countryCode', {
+//            lat: position.coords.latitude,
+//            lng: position.coords.longitude,
+//            type: 'JSON'
+//        }, function(result) {
+//            alert('Country: ' + result.countryName + '\n' + 'Code: ' + result.countryCode);
+//        });
+//    });
+//}​
